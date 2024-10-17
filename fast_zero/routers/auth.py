443ecd13285572675b157703bb3c0a952b82904c
Douglas paz <116456190+Douglasgls@users.noreply.github.com>
2024-10-17 16:36:04 -0300

    Update auth.py

--- fast_zero/routers/auth.py
+++ fast_zero/routers/auth.py
@@ -54,6 +54,6 @@ def login_for_access_token(
 def refresh_access_token(
     user: User = Depends(get_current_user),
 ):
-    new_access_token = create_access_token(data={'sub': user.email})
+    new_access_token = create_access_token(data={'sub': user.username})
 
     return {'access_token': new_access_token, 'token_type': 'bearer'}
